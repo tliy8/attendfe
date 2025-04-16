4612d22460565402ff8b85bfedc0aaae492c5a8b
tanliyang <agmenglei@gmail.com>
2025-04-16 17:24:36 +0800

    done all

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -82,6 +82,12 @@ export default function Admin() {
             >
               Add User
             </button>
+            <button
+            onClick={() => router.push("/allattendance")}
+            className="mt-2 w-full py-2 px-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md transition"
+            >
+          View All Attendance
+        </button>
           </div>
         )}
       </div>
